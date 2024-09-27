4bbda3f3aadf22f621a58f1aa7d4e6b810402649
GannouniAmine <mohamedamine.gannouni@ensi-uma.tn>
2024-09-27 22:21:04 +0100

    update

--- src/app/services/music/music.service.ts
+++ src/app/services/music/music.service.ts
@@ -10,7 +10,7 @@ export class MusicService {
     { id: 1, name: 'Die With A Smile', artist: 'Lady Gaga & Bruno Mars', imageUrl: 'assets/Lady_Gaga_and_Bruno_Mars_-_Die_with_a_Smile.png' },
     { id: 2, name: 'Girl I am Gonna Miss You', artist: 'Milli Vanilli', imageUrl: 'assets/Girl I am Gonna Miss You.webp' },
     { id: 3, name: 'Good Luck, Babe!', artist: 'Chappell Roan', imageUrl: 'assets/god luck baby.png' },
-    { id: 1, name: 'Godzella', artist: 'Eminem', imageUrl: 'assets/godzella.jpg' },
+    { id: 1, name: 'Godzella', artist: 'Eminem', imageUrl: 'assets/godzella.webp' },
     { id: 2, name: 'without me', artist: 'Eminem', imageUrl: 'assets/without me.jpg' },
     { id: 3, name: 'Bella', artist: 'Maître Gims', imageUrl: 'assets/bella.jpg' }
   ];
